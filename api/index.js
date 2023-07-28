2a55a7f0f3be7929d58f9edf37c438fabff72c47
manishkumar.singh <manish.singh@trainingmug.com>
2023-07-29 00:07:56 +0530

    local env

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 require("dotenv").config()
 
+require("dotenv").config({ path: `.env.local`, override: true })
+
 const app = express()
 const PORT = process.env.PORT || 4000
 const mongoose = require("mongoose")
