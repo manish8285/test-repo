f6995e3ddd1467dbab5559c53453b033a971cbd4
manishkumar.singh <manish.singh@trainingmug.com>
2023-07-28 23:23:57 +0530

    env added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const express = require("express")
+require("dotenv").config()
+
 const app = express()
 const PORT = process.env.PORT || 4000
 const mongoose = require("mongoose")
 const URL2 = "mongodb+srv://ermaanish:manish_singh@cluster0.5okciwd.mongodb.net/?retryWrites=true&w=majority" 
 
-mongoose.connect(URL2,{
+console.log(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
