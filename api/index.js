b10a7205f49cd20452ad802c83aa07682c2ca7b3
manishkumar.singh <manish.singh@trainingmug.com>
2023-07-29 00:27:55 +0530

    v changes

--- api/index.js
+++ api/index.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 4000
 const mongoose = require("mongoose")
 const URL2 = "mongodb+srv://ermaanish:manish_singh@cluster0.5okciwd.mongodb.net/?retryWrites=true&w=majority" 
 
-console.log(process.env.DB_URI)
-mongoose.connect(process.env.DB_URI,{
+//console.log(process.env.DB_URI)
+mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
     console.log("Connected to databases")
-}).catch(e=>console.log("connection to database failed"))
+}).catch(e=>console.log("connection to database failed"+e))
 
 
 const userSchema= mongoose.Schema({name:{
